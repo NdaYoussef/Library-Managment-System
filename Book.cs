06b10fc30ccb465c500a48d67d8019a2a39fbe1a
NdaYoussef <ndayou99@gmail.com>
2026-01-21 11:16:36 +0200

    Add class book and first properties

---
+++ Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Managment_System
+{
+    internal class Book
+    {
+        private int Id; 
+        private string Title;
+        private string Author;
+        private bool IsAvailable = true;
+
+
+        public Book()
+        {
+             
+
+        }
+    }
+}
