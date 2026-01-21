4f40239a0544b39b4c21eada4e03a5bd2464d789
NdaYoussef <ndayou99@gmail.com>
2026-01-21 03:48:13 +0200

    add basic code of program.cs file

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Library_Managment_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Library system!");
+           
+            while (true)
+            {
+                Console.WriteLine("Please choose to Add book (1) / Remove book (2) / Display book (3)");
+                char userChoise = Console.ReadLine()!.ToUpper()[0];
+                switch(userChoise)
+                {
+                    case '1':
+                break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    case '5':
+                        break;
+                    case '6':
+                        break;
+                    case '7':
+                        break;
+                    case '8':
+                        break;
+                    case '9':
+                        break;
+                }
+
+        }
+
+        }
+    }
+}
