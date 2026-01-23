43b941fa777ead3e6efdffbaf263dd6f5534f7e4
Wael Al-Bakry <waelbk96@gmail.com>
2026-01-23 20:08:11 +0200

    Library class Complete May need some minor mod.

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Managment_System
+{
+    internal class Book
+    {
+        public int Id;
+        public string Title;
+        public string Author;
+        public bool IsAvailable;
+
+        public Book(int id, string title, string author)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            IsAvailable = true;
+        }
+    }
+}
---
+++ Member.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Managment_System
+{
+    internal class Member
+    {
+        public int Id;
+        public string Name;
+
+        public Book[] BorrowedBooks;
+        public int BorrowedCount;
+
+        public Member(int id, string name)
+        {
+            Id = id;
+            Name = name;
+
+            BorrowedBooks = new Book[5]; // أقصى عدد استعارة
+            BorrowedCount = 0;
+        }
+    }
+}
