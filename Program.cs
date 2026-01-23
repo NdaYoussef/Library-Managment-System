6b282b4cd91d4e88d4100891c4d4579dcf12dca3
Wael Al-Bakry <waelbk96@gmail.com>
2026-01-23 20:13:13 +0200

    Program is Ready to Run may need some minor edit

--- Program.cs
+++ Program.cs
@@ -1,39 +1,96 @@
-﻿namespace Library_Managment_System
+﻿    namespace Library_Managment_System
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Library system!");
-           
-            while (true)
+
+            Library library = new Library();
+            string choice;
+
+            do
             {
-                Console.WriteLine("Please choose to Add book (1) / Remove book (2) / Display book (3)");
-                char userChoise = Console.ReadLine()!.ToUpper()[0];
-                switch(userChoise)
+                Console.WriteLine("\nLibrary Management System");
+                Console.WriteLine("1. Add Book");
+                Console.WriteLine("2. Remove Book");
+                Console.WriteLine("3. Add Member");
+                Console.WriteLine("4. Remove Member");
+                Console.WriteLine("5. Borrow Book");
+                Console.WriteLine("6. Return Book");
+                Console.WriteLine("7. List Books");
+                Console.WriteLine("8. List Members");
+                Console.WriteLine("9. Exit");
+
+                Console.Write("Select option: ");
+                
+                choice = Console.ReadLine();
+
+                switch (choice)
                 {
-                    case '1':
-                break;
-                    case '2':
+                    case "1":
+                        Console.Write("Book ID: ");
+                        int bid = int.Parse(Console.ReadLine());
+                        Console.Write("Title: ");
+                        string title = Console.ReadLine();
+                        Console.Write("Author: ");
+                        string author = Console.ReadLine();
+                        library.AddBook(new Book(bid,title,author));
                         break;
-                    case '3':
+
+                    case "2":
+                        Console.Write("Book ID: ");
+                        library.RemoveBook(int.Parse(Console.ReadLine()));
                         break;
-                    case '4':
+
+                    case "3":
+                        Console.Write("Member ID: ");
+                        int mid = int.Parse(Console.ReadLine());
+                        Console.Write("Name: ");
+                        string name = Console.ReadLine();
+                        library.AddMember(mid, name);
                         break;
-                    case '5':
+
+                    case "4":
+                        Console.Write("Member ID: ");
+                        library.RemoveMember(int.Parse(Console.ReadLine()));
                         break;
-                    case '6':
+
+                    case "5":
+                        Console.Write("Book ID: ");
+                        int b1 = int.Parse(Console.ReadLine());
+                        Console.Write("Member ID: ");
+                        int m1 = int.Parse(Console.ReadLine());
+                        library.BorrowBook(b1, m1);
                         break;
-                    case '7':
+
+                    case "6":
+                        Console.Write("Book ID: ");
+                        int b2 = int.Parse(Console.ReadLine());
+                        Console.Write("Member ID: ");
+                        int m2 = int.Parse(Console.ReadLine());
+                        library.ReturnBook(b2, m2);
                         break;
-                    case '8':
+
+                    case "7":
+                        library.ListBooks();
                         break;
-                    case '9':
+
+                    case "8":
+                        library.ListMember();
+                        break;
+
+                    case "9":
+                        Console.WriteLine("Exit");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option.");
                         break;
                 }
 
+            } while (choice != "9" || choice == null);
         }
 
-        }
     }
 }
